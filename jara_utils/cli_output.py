74a231032bd13a41d32aacccc61c33e7d2368c0a
Serban Senciuc <senciuc.serban@gmail.com>
2023-08-11 00:22:19 +0200

    Update annotations.

--- jara_utils/cli_output.py
+++ jara_utils/cli_output.py
@@ -1,6 +1,6 @@
 import re
 import sys
-from typing import TextIO
+from typing import TextIO, Optional
 
 from jara_utils.constants import TextColor
 
@@ -11,7 +11,7 @@ class CLIOutput:
         self.error_stream = error_stream
         self.hl_pattern = re.compile(r'\[[\w\d\-.,\s<>\':]+\]')
 
-    def _wrap(self, message: str, color: str, highlights: list[str] | None = None) -> str:
+    def _wrap(self, message: str, color: str, highlights: Optional[list[str]] = None) -> str:
         message = f'{color}{message}{TextColor.ENDC}'
         if highlights is None:
             highlights = re.findall(self.hl_pattern, message)
@@ -25,20 +25,20 @@ class CLIOutput:
     def write_error(self, message: str, crlf: str) -> None:
         self.stream.write(message + crlf)
 
-    def debug(self, text: str, highlights: list[str] | None = None, crlf: str = '\n') -> None:
+    def debug(self, text: str, highlights: Optional[list[str]] = None, crlf: str = '\n') -> None:
         self.write_stream(self._wrap(text, TextColor.CYAN, highlights), crlf)
 
-    def info(self, text: str, highlights: list[str] | None = None, crlf: str = '\n') -> None:
+    def info(self, text: str, highlights: Optional[list[str]] = None, crlf: str = '\n') -> None:
         self.write_stream(self._wrap(text, TextColor.OKBLUE, highlights), crlf)
 
-    def success(self, text: str, highlights: list[str] | None = None, crlf: str = '\n') -> None:
+    def success(self, text: str, highlights: Optional[list[str]] = None, crlf: str = '\n') -> None:
         self.write_stream(self._wrap(text, TextColor.OKGREEN, highlights), crlf)
 
-    def warning(self, text: str, highlights: list[str] | None = None, crlf: str = '\n') -> None:
+    def warning(self, text: str, highlights: Optional[list[str]] = None, crlf: str = '\n') -> None:
         self.write_stream(self._wrap(text, TextColor.YELLOW, highlights), crlf)
 
-    def error(self, text: str, highlights: list[str] | None = None, crlf: str = '\n') -> None:
+    def error(self, text: str, highlights: Optional[list[str]] = None, crlf: str = '\n') -> None:
         self.write_error(self._wrap(text, TextColor.RED, highlights), crlf)
 
-    def fail(self, text: str, highlights: list[str] | None = None, crlf: str = '\n') -> None:
+    def fail(self, text: str, highlights: Optional[list[str]] = None, crlf: str = '\n') -> None:
         self.error(text, highlights, crlf)
